12badc66f15f0145cc5787cfbfc1d9d255c58e17
linnefromice <linnefromice@gmail.com>
2022-03-14 17:14:11 +0900

    enable to compile

--- contracts/truffle-config.js
+++ contracts/truffle-config.js
@@ -20,8 +20,8 @@
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
-const etherscanAPI = fs.readFileSync(".etherscanApi").toString().trim();
+const mnemonic = fs.existsSync(".secret") ? fs.readFileSync(".secret").toString().trim() : ""
+const etherscanAPI = fs.existsSync(".etherscanApi") ? fs.readFileSync(".etherscanApi").toString().trim() : ""
 
 module.exports = {
   /**
