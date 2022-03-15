707b6c85b498b4032679677ec3399f986132aa4c
linnefromice <linnefromice@gmail.com>
2022-03-16 01:42:35 +0900

    fix added mocks

--- contracts/contracts/mocks/MockCurveVoteEscrow.sol
+++ contracts/contracts/mocks/MockCurveVoteEscrow.sol
@@ -6,10 +6,13 @@ import "../Interfaces.sol";
 // ref: https://curve.readthedocs.io/dao-vecrv.html?highlight=CRV#curve-dao-vote-escrowed-crv
 contract MockCurveVoteEscrow {
   function create_lock(uint256 _value, uint256 _unlock_time) external {}
+
   function increase_amount(uint256 _amount) external {}
+
   function increase_unlock_time(uint256 _unlock_time) external {}
+
   function withdraw() external {}
-  
+
   function smart_wallet_checker() external view returns (address) {
     return 0x0000000000000000000000000000000000000000;
   }
