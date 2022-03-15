e3cdd34ea708c6e32db5df87c8ab9720d29c31b3
linnefromice <linnefromice@gmail.com>
2022-03-16 01:40:14 +0900

    add mock for Curve LiquidityGauge

---
+++ contracts/contracts/mocks/MockCurveGauge.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "../Interfaces.sol";
+
+// refs
+// - https://curve.readthedocs.io/dao-gauges.html?highlight=reward_tokens#liquiditygaugev2
+// - https://curve.readthedocs.io/dao-gauges.html?highlight=reward_tokens#liquiditygaugereward
+contract MockCurveGauge {
+    function deposit(uint256 _amount) external {}
+
+    function balanceOf(address _address) external view returns (uint256) {
+      return 0;
+    }
+    function withdraw(uint256 _amount) external {}
+    
+    function claim_rewards() external {}
+
+    function reward_tokens(uint256 _amount) external view returns(address) { //v2
+      return 0x0000000000000000000000000000000000000000;
+    }
+    function rewarded_token() external view returns(address) { //v1
+      return 0x0000000000000000000000000000000000000000;
+    }
+    function lp_token() external view returns(address) {
+      return 0x0000000000000000000000000000000000000000;
+    }
+}
