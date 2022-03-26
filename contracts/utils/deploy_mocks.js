40336ca8eb41c6bddfdf01664399513e01669909
linnefromice <linnefromice@gmail.com>
2022-03-26 23:05:51 +0900

    update utils - add deployment function for kgl protocol

--- contracts/utils/deploy_mocks.js
+++ contracts/utils/deploy_mocks.js
@@ -1,3 +1,4 @@
+// tokens
 const _deployMintableERC20 = async ({
   name,
   symbol,
@@ -47,9 +48,65 @@ const deployWethToken = async ({ deployer, artifacts }) =>
     artifacts: artifacts,
   })
 
+// Curve
+const deployMockKaglaGauge = async ({
+  threeKglTokenAddress,
+  deployer,
+  artifacts,
+}) => {
+  const MockKaglaGauge = artifacts.require('MockKaglaGauge')
+  return await MockKaglaGauge.new(threeKglTokenAddress, {
+    from: deployer,
+  })
+}
+
+const deployMockKaglaVotingEscrow = async ({ deployer, artifacts }) =>
+  await artifacts.require('MockKaglaVoteEscrow').new({ from: deployer })
+
+const deployMockKaglaRegistry = async ({
+  pool,
+  gauge,
+  lpToken,
+  deployer,
+  artifacts,
+}) => {
+  const Artifact = artifacts.require('MockKaglaRegistry')
+  return await Artifact.new(pool, gauge, lpToken, {
+    from: deployer,
+  })
+}
+
+const deployMockKaglaFeeDistributor = async ({
+  tokenAddress,
+  deployer,
+  artifacts,
+}) => {
+  const Artifact = artifacts.require('MockKaglaFeeDistributor')
+  return await Artifact.new(tokenAddress, {
+    from: deployer,
+  })
+}
+
+const deployMockKaglaAddressProvider = async ({
+  registryAddress,
+  feeDistributorAddress,
+  deployer,
+  artifacts,
+}) => {
+  const Artifact = artifacts.require('MockKaglaAddressProvider')
+  return await Artifact.new(registryAddress, feeDistributorAddress, {
+    from: deployer,
+  })
+}
+
 module.exports = {
   deployKglToken: deployKglToken,
   deployThreeKglToken: deployThreeKglToken,
   deployDaiToken: deployDaiToken,
   deployWethToken: deployWethToken,
+  deployMockKaglaGauge: deployMockKaglaGauge,
+  deployMockKaglaVotingEscrow: deployMockKaglaVotingEscrow,
+  deployMockKaglaRegistry: deployMockKaglaRegistry,
+  deployMockKaglaFeeDistributor: deployMockKaglaFeeDistributor,
+  deployMockKaglaAddressProvider: deployMockKaglaAddressProvider,
 }
