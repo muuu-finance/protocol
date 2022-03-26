51f83263c13dcd9a0c6db5f215bfceb0f4451d42
linnefromice <linnefromice@gmail.com>
2022-03-26 22:28:57 +0900

    fix script to deploy mocks - pass artifacts

--- contracts/utils/deploy_mocks.js
+++ contracts/utils/deploy_mocks.js
@@ -1,40 +1,50 @@
-const MintableERC20 = artifacts.require('MintableERC20')
-
-const _deployMintableERC20 = async ({ name, symbol, decimals, deployer }) =>
-  await MintableERC20.new(name, symbol, decimals, {
+const _deployMintableERC20 = async ({
+  name,
+  symbol,
+  decimals,
+  deployer,
+  artifacts,
+}) => {
+  const MintableERC20 = artifacts.require('MintableERC20')
+  return await MintableERC20.new(name, symbol, decimals, {
     from: deployer,
   })
+}
 
-const deployKglToken = async ({ deployer }) =>
+const deployKglToken = async ({ deployer, artifacts }) =>
   await _deployMintableERC20({
     name: 'Kagle DAO Token',
     symbol: 'KGL',
     decimals: 18,
     deployer: deployer,
+    artifacts: artifacts,
   })
 
-const deployThreeKglToken = async ({ deployer }) =>
+const deployThreeKglToken = async ({ deployer, artifacts }) =>
   await _deployMintableERC20({
     name: 'Kagla.fi DAI/USDC/USDT',
     symbol: '3Kgl',
     decimals: 18,
     deployer: deployer,
+    artifacts: artifacts,
   })
 
-const deployDaiToken = async ({ deployer }) =>
+const deployDaiToken = async ({ deployer, artifacts }) =>
   await _deployMintableERC20({
     name: 'Dai Stablecoin',
     symbol: 'DAI',
     decimals: 18,
     deployer: deployer,
+    artifacts: artifacts,
   })
 
-const deployWethToken = async ({ deployer }) =>
+const deployWethToken = async ({ deployer, artifacts }) =>
   await _deployMintableERC20({
     name: 'Wrapped Ether',
     symbol: 'WETH',
     decimals: 18,
     deployer: deployer,
+    artifacts: artifacts,
   })
 
 module.exports = {
