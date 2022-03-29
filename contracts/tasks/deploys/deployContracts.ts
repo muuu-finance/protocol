82f7a3ae0566f8c785e41d8781b5dccbc46e285c
linnefromice <linnefromice@gmail.com>
2022-03-29 20:30:03 +0900

    use typechain in task

--- contracts/tasks/deploys/deployContracts.ts
+++ contracts/tasks/deploys/deployContracts.ts
@@ -1,5 +1,6 @@
 import { task } from "hardhat/config"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { MuKglToken__factory, MuuuToken__factory } from "../../types"
 
 task("deploy-contracts", "Deploy contracts").setAction(async ({}, hre: HardhatRuntimeEnvironment) => {
   const { network, ethers } = hre
@@ -7,18 +8,15 @@ task("deploy-contracts", "Deploy contracts").setAction(async ({}, hre: HardhatRu
   const [signer] = await ethers.getSigners()
   console.log(`deployer: ${await signer.getAddress()}`)
 
-  const MuuuToken = await ethers.getContractFactory("MuuuToken")
-  const muuuToken = await MuuuToken.deploy(ethers.constants.AddressZero);
-  await muuuToken.deployed();
+  const muuuToken = await new MuuuToken__factory(signer).deploy(ethers.constants.AddressZero)
+  await muuuToken.deployTransaction.wait()
   console.log(`MuuuToken deployed to: ${muuuToken.address}`);
 
-  const MuKglToken = await ethers.getContractFactory("MuKglToken")
-  const muKglToken = await MuKglToken.deploy();
-  await muKglToken.deployed();
+  const muKglToken = await new MuKglToken__factory(signer).deploy();
+  await muuuToken.deployTransaction.wait()
   console.log(`MuKglToken deployed to: ${muKglToken.address}`);
 
-  // const MuuuLockerV2 = await ethers.getContractFactory("MuuuLockerV2")
-  // const muuuLockerV2 = await MuuuLockerV2.deploy();
-  // await muuuLockerV2.deployed();
+  // const muuuLockerV2 = await new MuuuLockerV2__factory(signer).deploy();
+  // await muuuLockerV2.deployTransaction.wait()
   // console.log(`MuuuLockerV2 deployed to: ${muuuLockerV2.address}`);
 })
