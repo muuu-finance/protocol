f2174c6b0765541136d1080159d6231f34c7e88a
linnefromice <linnefromice@gmail.com>
2022-03-29 16:53:03 +0900

    minor collection

--- contracts/tasks/samples/accounts.ts
+++ contracts/tasks/samples/accounts.ts
@@ -6,6 +6,8 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 task('accounts', 'Prints the list of accounts').setAction(async ({}, hre: HardhatRuntimeEnvironment) => {
   const accounts: SignerWithAddress[]  = await hre.ethers.getSigners()
   for (const account of accounts) {
-    console.log(await account.address)
+    console.log(`------------------------------------------`)
+    console.log(`account ... ${await account.address}`)
+    console.log(`balance ... ${await account.getBalance()}`)
   }
 })
