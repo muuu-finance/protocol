8d832f2fd5e37d6bb3f4c3388cfbe9902b346ae6
linnefromice <linnefromice@gmail.com>
2022-04-14 14:25:28 +0900

    add deploy task - MuuuStakingProxyV2

---
+++ contracts/tasks/deploys/deploy-MuuuStakingProxyV2.ts
@@ -0,0 +1,78 @@
+import { task } from 'hardhat/config'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { deployMuuuStakingProxyV2 } from '../../helpers/contracts-deploy-helpers'
+import { loadConstants } from '../constants'
+import { ContractJsonGroups, ContractKeys, TaskUtils } from '../utils'
+
+const CONTRACT_KEY = ContractKeys.MuuuStakingProxyV2
+task(`deploy-${CONTRACT_KEY}`, `Deploy ${CONTRACT_KEY}`)
+  .addOptionalParam('deployerAddress', "Deployer's address")
+  .addFlag('inMultiDeploymentFlow', 'Whether in a flow to multi deployments')
+  .addFlag(
+    'useAlreadyDeployed',
+    'Use already deployed contracts, get addresses from json to have deployed contract addresses',
+  )
+  .addFlag(
+    'useMockContracts',
+    'Use mock contracts, get addresses from json to mock contract addresses',
+  )
+  .setAction(
+    async (
+      {
+        deployerAddress,
+        inMultiDeploymentFlow,
+        useAlreadyDeployed,
+        useMockContracts,
+      }: {
+        deployerAddress: string
+        inMultiDeploymentFlow: boolean
+        useAlreadyDeployed: boolean
+        useMockContracts: boolean
+      },
+      hre: HardhatRuntimeEnvironment,
+    ) => {
+      const { network, ethers } = hre
+      const _deployer =
+        (await ethers.getSigner(deployerAddress)) ||
+        (await ethers.getSigners())[0]
+      if (!inMultiDeploymentFlow) {
+        console.log(`--- [deploy-${CONTRACT_KEY}] START ---`)
+        console.log(`network: ${network.name}`)
+        console.log(`useAlreadyDeployed flag: ${useAlreadyDeployed}`)
+      }
+
+      // get constants / addresses / parameters
+      const deployeds = TaskUtils.loadDeployedContractAddresses({
+        network: network.name,
+      })
+      const constants = loadConstants({
+        network: network.name,
+        isUseMocks: useMockContracts,
+      })
+
+      console.log(`> start deploy ${CONTRACT_KEY}`)
+
+      const instance = await deployMuuuStakingProxyV2({
+        deployer: _deployer,
+        rewards: deployeds.system.muuuLockerV2,
+        kgl: constants.tokens.KGL,
+        muuu: deployeds.system.muuu,
+        muKgl: deployeds.system.muKgl,
+        muuuStaking: deployeds.system.muuuRewards,
+        muKglStaking: deployeds.system.muKglRewards,
+        kglDeposit: deployeds.system.kglDepositor,
+      })
+      TaskUtils.writeContractAddress({
+        group: ContractJsonGroups.system,
+        name: 'muuuStakingProxyV2',
+        value: instance.address,
+        fileName: TaskUtils.getFilePath({ network: network.name }),
+      })
+
+      console.log(`>> deployed ${CONTRACT_KEY}\n`)
+
+      if (!inMultiDeploymentFlow)
+        console.log(`--- [deploy-${CONTRACT_KEY}] FINISHED ---`)
+      return instance.address
+    },
+  )
