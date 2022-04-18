aceef44f53e789bba4de0f8521237aa427385a65
linnefromice <linnefromice@gmail.com>
2022-04-18 16:02:35 +0900

    update VotingBalanceV2Gauges - remove consideration for old locker

--- contracts/contracts/VotingBalanceV2Gauges.sol
+++ contracts/contracts/VotingBalanceV2Gauges.sol
@@ -5,24 +5,14 @@ pragma experimental ABIEncoderV2;
 import "./interfaces/ILockedMuuu.sol";
 
 contract VotingBalanceV2Gauges {
-  address public constant oldlocker = address(0xD18140b4B819b895A3dba5442F959fA44994AF50);
   address public locker;
   uint256 public constant rewardsDuration = 86400 * 7;
   uint256 public constant lockDuration = rewardsDuration * 17;
-
-  bool public UseOldLocker = true;
-  address public constant owner = address(0xa3C5A1e09150B75ff251c1a7815A07182c3de2FB);
 
   constructor(
     address _locker
   ) public {
     _locker = locker;
-  }
-
-  function setUseOldLocker(bool _use) external {
-    require(msg.sender == owner, "!auth");
-
-    UseOldLocker = _use;
   }
 
   function balanceOf(address _account) external view returns (uint256) {
@@ -56,21 +46,6 @@ contract VotingBalanceV2Gauges {
 
     //get pending
     uint256 pending = ILockedMuuu(locker).pendingLockAtEpochOf(epochindex, _account);
-
-    //if using old locker for grace period
-    if (UseOldLocker) {
-      //check if tokens have not been withdrawn yet
-      if (ILockedMuuu(oldlocker).lockedBalanceOf(_account) > 0) {
-        uint256 eindex = ILockedMuuu(oldlocker).epochCount() - 1;
-        (, uint32 _edate) = ILockedMuuu(oldlocker).epochs(eindex);
-        if (_edate >= currentEpoch) {
-          //if end date is already the current epoch,  minus 1 to get the previous
-          eindex--;
-        }
-        //add to current balance
-        pending += ILockedMuuu(oldlocker).balanceAtEpochOf(eindex, _account);
-      }
-    }
 
     return balanceAtPrev + pending;
   }
