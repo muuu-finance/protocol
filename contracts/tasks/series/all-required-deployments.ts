3fad11953a3da493141286bb3336c124d065566b
Shuto kikuchi <mizoren8@gmail.com>
2022-04-21 23:10:30 +0900

    update: addMinter

--- contracts/tasks/series/all-required-deployments.ts
+++ contracts/tasks/series/all-required-deployments.ts
@@ -576,7 +576,7 @@ task(
       await _addBoosterToMinter({
         signer,
         muuuTokenAddress: muuuTokenAddress,
-        minterAddress: boosterAddress,
+        minterAddress: kaglaVoterProxyAddress,
       })
 
       const { rewardFactoryAddress, tokenFactoryAddress, stashFactoryAddress } =
