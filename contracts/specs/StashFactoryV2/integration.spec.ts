c008013caf9167a93099802c261a1a33b6a2519e
linnefromice <linnefromice@gmail.com>
2022-04-21 16:38:55 +0900

    update specs

--- contracts/specs/StashFactoryV2/integration.spec.ts
+++ contracts/specs/StashFactoryV2/integration.spec.ts
@@ -2,7 +2,7 @@ import { expect } from "chai"
 import { Signer, Wallet } from "ethers";
 import { ethers } from "hardhat"
 import * as DeployHelper from "../../helpers/contracts-deploy-helpers";
-import { ERC20__factory, StashFactoryV2__factory } from "../../types"
+import { Booster, ERC20__factory, ExtraRewardStashV3__factory, ProxyFactory__factory, StashFactoryV2, StashFactoryV2__factory } from "../../types"
 
 const createRandomAddress = () => Wallet.createRandom().address;
 
@@ -15,10 +15,16 @@ const fullSetup = async () => {
     'KGL',
   )
   await kgl.deployTransaction.wait()
-  const staker = createRandomAddress()
+  const voterProxy = await DeployHelper.deployKaglaVoterProxy({
+    deployer,
+    kgl: kgl.address,
+    votingEscrow: ethers.constants.AddressZero,
+    gaugeController: ethers.constants.AddressZero,
+    tokenMinter: ethers.constants.AddressZero,
+  })
   const booster = await DeployHelper.deployBooster({
     deployer,
-    staker,
+    staker: voterProxy.address,
     minter: ethers.constants.AddressZero,
     kgl: kgl.address,
     registry: ethers.constants.AddressZero,
@@ -34,14 +40,17 @@ const fullSetup = async () => {
     deployer,
     operator: booster.address,
   })
+  const proxyFactory = await new ProxyFactory__factory(deployer).deploy()
+  await proxyFactory.deployTransaction.wait()
   const sFactory = await new StashFactoryV2__factory(deployer).deploy(
     booster.address,
     rFactory.address,
-    ethers.constants.AddressZero
+    proxyFactory.address
   )
   await sFactory.deployTransaction.wait()
 
-  // setup Booster to enable to add pool
+  // setup Booster / VoterProxy to enable to add pool
+  await (await voterProxy.setOperator(booster.address))
   await (await booster.setPoolManager(deployer.address))
   await (await booster.setFactories(
     rFactory.address,
@@ -55,6 +64,20 @@ const fullSetup = async () => {
     rFactory,
     sFactory,
     tFactory,
+  }
+}
+
+const setImplementationToStashFactoryV2 = async ({stashFactoryV2, operator, deployer}: {stashFactoryV2: StashFactoryV2, operator: Signer, deployer: Signer}) => {
+  const _instance = stashFactoryV2.connect(operator)
+  const v3 = await new ExtraRewardStashV3__factory(deployer).deploy()
+  await v3.deployTransaction.wait()
+  await (await _instance.setImplementation(
+    ethers.constants.AddressZero,
+    ethers.constants.AddressZero,
+    v3.address
+  ))
+  return {
+    v3Implementation: v3
   }
 }
 
@@ -73,5 +96,27 @@ describe('StashFactoryV2 - integration', () => {
     expect(rewardFactory).to.equal(rFactory.address)
     expect(tokenFactory).to.equal(tFactory.address)
     expect(stashFactory).to.equal(sFactory.address)
+  })
+
+  describe("StashFactoryV2#CreateStash", () => {
+    it("success", async () => {
+      const { booster, deployer, sFactory } = await fullSetup()
+      await setImplementationToStashFactoryV2({
+        stashFactoryV2: sFactory,
+        operator: deployer,
+        deployer
+      })
+      
+      const lpToken = await new ERC20__factory(deployer).deploy(
+        'Dummy LP Token',
+        'DUMMY',
+      )
+      const gauge = createRandomAddress()
+      await expect(booster.connect(deployer).addPool(
+        lpToken.address,
+        gauge,
+        3
+      )).to.emit(booster, "PoolAdded").withArgs(lpToken.address, gauge, 3)
+    })
   })
 })
