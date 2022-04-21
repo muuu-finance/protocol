5bd3b2a9721a6fce62f9d9cca716ee70c8dc8010
linnefromice <linnefromice@gmail.com>
2022-04-21 14:56:53 +0900

    update spec

--- contracts/specs/StashFactoryV2.spec.ts
+++ contracts/specs/StashFactoryV2.spec.ts
@@ -1,13 +1,16 @@
 import { expect } from "chai"
+import { Wallet } from "ethers";
 import { ethers } from "hardhat"
 import { StashFactoryV2__factory } from "../types"
 
-const prepare = async () => {
+const createRandomAddress = () => Wallet.createRandom().address;
+
+const prepare = async ({ operator, rewardFactory, proxyFactory }: { operator?: string, rewardFactory?: string, proxyFactory?: string } = {}) => {
   const [deployer] = await ethers.getSigners()
   const _instance = await new StashFactoryV2__factory(deployer).deploy(
-    ethers.constants.AddressZero,
-    ethers.constants.AddressZero,
-    ethers.constants.AddressZero
+    operator || ethers.constants.AddressZero,
+    rewardFactory || ethers.constants.AddressZero,
+    proxyFactory || ethers.constants.AddressZero
   )
   await _instance.deployTransaction.wait()
   return {
@@ -18,7 +21,14 @@ const prepare = async () => {
 
 describe('StashFactoryV2', () => {
   it(".constructor", async () => {
-    const { stashFactoryV2 } = await prepare()
+    const inputtedOperator = createRandomAddress();
+    const inputtedRewardFactory = createRandomAddress();
+    const inputtedProxyFactory = createRandomAddress();
+    const { stashFactoryV2 } = await prepare({
+      operator: inputtedOperator,
+      rewardFactory: inputtedRewardFactory,
+      proxyFactory: inputtedProxyFactory
+    })
     const [operator, rewardFactory, proxyFactory, v1Impl, v2Impl, v3Impl] = await Promise.all([
       stashFactoryV2.operator(),
       stashFactoryV2.rewardFactory(),
@@ -27,9 +37,9 @@ describe('StashFactoryV2', () => {
       stashFactoryV2.v2Implementation(),
       stashFactoryV2.v3Implementation(),
     ])
-    expect(operator).to.equal(ethers.constants.AddressZero)
-    expect(rewardFactory).to.equal(ethers.constants.AddressZero)
-    expect(proxyFactory).to.equal(ethers.constants.AddressZero)
+    expect(operator).to.equal(inputtedOperator)
+    expect(rewardFactory).to.equal(inputtedRewardFactory)
+    expect(proxyFactory).to.equal(inputtedProxyFactory)
     expect(v1Impl).to.equal(ethers.constants.AddressZero)
     expect(v2Impl).to.equal(ethers.constants.AddressZero)
     expect(v3Impl).to.equal(ethers.constants.AddressZero)
