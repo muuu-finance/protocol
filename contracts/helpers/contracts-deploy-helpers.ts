9fe8e7391ff33d7abdcc5c9028f321c6a053faf2
linnefromice <linnefromice@gmail.com>
2022-04-22 01:55:07 +0900

    update deploy helpers

--- contracts/helpers/contracts-deploy-helpers.ts
+++ contracts/helpers/contracts-deploy-helpers.ts
@@ -14,6 +14,9 @@ import {
   MuuuRewardPool__factory,
   MuuuStakingProxyV2__factory,
   MuuuToken__factory,
+  PoolManagerProxy__factory,
+  PoolManagerSecondaryProxy__factory,
+  PoolManagerV3__factory,
   PoolManager__factory,
   ProxyFactory__factory,
   RewardFactory__factory,
@@ -257,6 +260,43 @@ export const deployPoolManager = async ({
     await new PoolManager__factory(deployer).deploy(pools, addressProvider),
     ContractKeys.PoolManager,
   )
+
+export const deployPoolManagerProxy = async ({
+  deployer,
+  pools
+}: DeployCommonArgs & {
+  pools: string
+}) => withSaveAndVerify(
+  await new PoolManagerProxy__factory(deployer).deploy(pools),
+  ContractKeys.PoolManagerProxy,
+)
+
+export const deployPoolManagerSecondaryProxy = async ({
+  deployer,
+  gaugeController,
+  pools,
+  booster
+}: DeployCommonArgs & {
+  gaugeController: string
+  pools: string
+  booster: string
+}) => withSaveAndVerify(
+  await new PoolManagerSecondaryProxy__factory(deployer).deploy(gaugeController, pools, booster),
+  ContractKeys.PoolManagerSecondaryProxy,
+)
+
+export const deployPoolManagerV3 = async ({
+  deployer,
+  pools,
+  gaugeController,
+}: DeployCommonArgs & {
+  pools: string
+  gaugeController: string
+}) => withSaveAndVerify(
+  await new PoolManagerV3__factory(deployer).deploy(pools, gaugeController),
+  ContractKeys.PoolManagerV3,
+)
+
 
 export const deployArbitratorVault = async ({
   deployer,
