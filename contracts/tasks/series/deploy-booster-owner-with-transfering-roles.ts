98f77c05e60232c307dcb0909d8b681d87dfcaa6
linnefromice <linnefromice@gmail.com>
2022-04-22 17:35:36 +0900

    update deploy-booster-owner-with-transfering-roles

--- contracts/tasks/series/deploy-booster-owner-with-transfering-roles.ts
+++ contracts/tasks/series/deploy-booster-owner-with-transfering-roles.ts
@@ -1,6 +1,7 @@
 import { task } from 'hardhat/config'
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { TaskUtils } from '../utils';
+import { BoosterOwner__factory, Booster__factory } from '../../types';
+import { ContractKeys, TaskUtils } from '../utils';
 
 task(
   'deploy-booster-owner-with-transfering-roles',
@@ -17,11 +18,28 @@ task(
   })
 
   console.log(`--- start: deployments ---`)
-  // TODO
+  const boosterOwnerAddress = await hre.run(
+    `deploy-${ContractKeys.BoosterOwner}`,
+    {
+      deployerAddress: signer.address,
+      inMultiDeploymentFlow: true,
+      useAlreadyDeployed: false,
+    }
+  )
   console.log(`--- finish: deployments ---`)
 
   console.log(`--- start: transfer role ---`)
-  // TODO
+  const boosterOwner = BoosterOwner__factory.connect(boosterOwnerAddress, signer)
+  const booster = Booster__factory.connect(deployeds.system.booster, signer)
+  await (await booster.transferOwnership(boosterOwnerAddress))
+  const newBoosterOwner = await booster.owner()
+  console.log(`check: booster's owner = ${newBoosterOwner}`)
+  if (newBoosterOwner.toLowerCase() != boosterOwnerAddress.toLowerCase()) {
+    console.log("[ERROR] Premine total does not match to calculated total from holders")
+    throw new Error("[ERROR] current booster's owner is not BoosterOwner address after Booster#transferOwner")
+  }
+  // TODO: transfer BoosterOwner's owner to admin address?
+  await (await boosterOwner.sealOwnership())
   console.log(`--- finish: transfer role ---`)
 
   console.log(
