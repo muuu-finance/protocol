6953e261a69cc3e9c6186f975507d42158a4624a
linnefromice <linnefromice@gmail.com>
2022-04-23 23:21:44 +0900

    update task - transfer-roles

--- contracts/tasks/series/transfer-roles.ts
+++ contracts/tasks/series/transfer-roles.ts
@@ -1,10 +1,27 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "ethers";
 import { task } from "hardhat/config";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { BoosterOwner__factory, Booster__factory, KaglaVoterProxy__factory, KglDepositor__factory, MuuuLockerV2__factory, MuuuStakingProxyV2__factory, MuuuToken__factory, PoolManagerSecondaryProxy__factory, PoolManagerV3__factory, TreasuryFunds__factory } from "../../types";
 import { TaskUtils } from "../utils";
 
+// Parameters to use in task
+const ADDRESSES_PARAMS = {
+  operatorInTreasury: "",
+  ownerInBoosterOwner: "",
+  voteDelegateInBooster: "",
+  feeManagerInBooster: "",
+  ownerInKagleVoterProxy: "",
+  ownerInKglDepositor: "",
+  feeManagerInKglDepositor: "",
+  ownerInMuuuToken: "",
+  ownerInMuuuLockerV2: "",
+  ownerInMuuuStakingProxyV2: "",
+  operatorInPoolManagerV3: "",
+  ownerInPoolManagerSecondaryProxy: ""
+}
 
+// Utilities
 type Addresses = {
   treasuryFunds: string
   booster: string
@@ -18,35 +35,75 @@ type Addresses = {
   poolManagerSecondaryProxy: string
 }
 
+const _generateContracts = (provider: SignerWithAddress | ethers.providers.JsonRpcProvider, addrs: Addresses) => ({
+  _treasuryFunds: TreasuryFunds__factory.connect(addrs.treasuryFunds, provider),
+  _booster: Booster__factory.connect(addrs.booster, provider),
+  _boosterOwner: BoosterOwner__factory.connect(addrs.boosterOwner, provider),
+  _kaglaVoterProxy: KaglaVoterProxy__factory.connect(addrs.kaglaVoterProxy, provider),
+  _kglDepositor: KglDepositor__factory.connect(addrs.kglDepositor, provider),
+  _muuuToken: MuuuToken__factory.connect(addrs.muuuToken, provider),
+  _muuuLockerV2: MuuuLockerV2__factory.connect(addrs.muuuLockerV2, provider),
+  _muuuStakingProxyV2: MuuuStakingProxyV2__factory.connect(addrs.muuuStakingProxyV2, provider),
+  _poolManagerV3: PoolManagerV3__factory.connect(addrs.poolManagerV3, provider),
+  _poolManagerSecondaryProxy: PoolManagerSecondaryProxy__factory.connect(addrs.poolManagerSecondaryProxy, provider),
+})
+
 const getRoles = async (provider: ethers.providers.JsonRpcProvider, addrs: Addresses) => {
-  const _treasuryFunds = TreasuryFunds__factory.connect(addrs.treasuryFunds, provider)
-  const _booster = Booster__factory.connect(addrs.booster, provider)
-  const _boosterOwner = BoosterOwner__factory.connect(addrs.boosterOwner, provider)
-  const _kaglaVoterProxy = KaglaVoterProxy__factory.connect(addrs.kaglaVoterProxy, provider)
-  const _kglDepositor = KglDepositor__factory.connect(addrs.kglDepositor, provider)
-  const _muuuToken = MuuuToken__factory.connect(addrs.muuuToken, provider)
-  const _muuuLockerV2 = MuuuLockerV2__factory.connect(addrs.muuuLockerV2, provider)
-  const _muuuStakingProxyV2 = MuuuStakingProxyV2__factory.connect(addrs.muuuStakingProxyV2, provider)
-  const _poolManagerV3 = PoolManagerV3__factory.connect(addrs.poolManagerV3, provider)
-  const _poolManagerSecondaryProxy = PoolManagerSecondaryProxy__factory.connect(addrs.poolManagerSecondaryProxy, provider)
+  const contracts = _generateContracts(provider, addrs)
 
   const functions = [
-    { label: "Treasury’s operator", fn: _treasuryFunds.operator },
-    { label: "BoosterOwner's owner", fn: _boosterOwner.owner },
-    { label: "Booster’s voteDelegate", fn: _booster.voteDelegate },
-    { label: "Booster’s feeManager", fn: _booster.feeManager },
-    { label: "KaglaVoterProxy’s owner", fn: _kaglaVoterProxy.owner },
-    { label: "KglDepositor’s owner", fn: _kglDepositor.owner },
-    { label: "KglDepositor’s feeManager", fn: _kglDepositor.feeManager },
-    { label: "MuuuToken’s Owner", fn: _muuuToken.owner },
-    { label: "MuuuLockerV2’s owner", fn: _muuuLockerV2.owner },
-    { label: "MuuuStakingProxyV2’s owner", fn: _muuuStakingProxyV2.owner },
-    { label: "PoolManagerV3’s operator", fn: _poolManagerV3.operator },
-    { label: "PoolManagerSecondaryProxy’s owner", fn: _poolManagerSecondaryProxy.owner },
+    { label: "Treasury’s operator", fn: contracts._treasuryFunds.operator },
+    { label: "BoosterOwner's owner", fn: contracts._boosterOwner.owner },
+    { label: "Booster’s voteDelegate", fn: contracts._booster.voteDelegate },
+    { label: "Booster’s feeManager", fn: contracts._booster.feeManager },
+    { label: "KaglaVoterProxy’s owner", fn: contracts._kaglaVoterProxy.owner },
+    { label: "KglDepositor’s owner", fn: contracts._kglDepositor.owner },
+    { label: "KglDepositor’s feeManager", fn: contracts._kglDepositor.feeManager },
+    { label: "MuuuToken’s Owner", fn: contracts._muuuToken.owner },
+    { label: "MuuuLockerV2’s owner", fn: contracts._muuuLockerV2.owner },
+    { label: "MuuuStakingProxyV2’s owner", fn: contracts._muuuStakingProxyV2.owner },
+    { label: "PoolManagerV3’s operator", fn: contracts._poolManagerV3.operator },
+    { label: "PoolManagerSecondaryProxy’s owner", fn: contracts._poolManagerSecondaryProxy.owner },
   ]
   for (const _v of functions) console.log(`${_v.label} ... ${await _v.fn()}`)
 }
 
+const bulkTransferRoleToInputtedAddress = async (signer: SignerWithAddress, addrs: Addresses, params: typeof ADDRESSES_PARAMS) => {
+  const {
+    _treasuryFunds,
+    _booster,
+    // _boosterOwner,
+    _kaglaVoterProxy,
+    _kglDepositor,
+    _muuuToken,
+    _muuuLockerV2,
+    _muuuStakingProxyV2,
+    _poolManagerV3,
+    _poolManagerSecondaryProxy,
+  } = _generateContracts(signer, addrs)
+
+  await (await _treasuryFunds.setOperator(params.operatorInTreasury)).wait()
+  await (await _booster.setVoteDelegate(params.voteDelegateInBooster)).wait()
+  await (await _booster.setFeeManager(params.feeManagerInBooster)).wait()
+  await (await _kaglaVoterProxy.transferOwnership(params.ownerInKagleVoterProxy)).wait()
+  await (await _kglDepositor.setFeeManager(params.feeManagerInKglDepositor)).wait()
+  await (await _kglDepositor.transferOwnership(params.ownerInKglDepositor)).wait()
+  await (await _muuuToken.transferOwnership(params.ownerInMuuuToken)).wait()
+  await (await _muuuLockerV2.transferOwnership(params.ownerInMuuuLockerV2)).wait()
+  
+  // transfer MuuuStakingProxyV2's owner
+  await (await _muuuStakingProxyV2.setPendingOwner(params.ownerInMuuuStakingProxyV2)).wait()
+  await (await _muuuStakingProxyV2.applyPendingOwner()).wait()
+
+  await (await _poolManagerV3.setOperator(params.operatorInPoolManagerV3)).wait()
+  await (await _poolManagerSecondaryProxy.setOwner(params.ownerInPoolManagerSecondaryProxy)).wait()
+
+  // About BoosterOwner
+  // BoosterOwner#transferOwnership from old admin
+  // BoosterOwner#acceptOwnership from new admin
+}
+
+// Main
 task(
   'transfer-roles',
   "Transfer contract roles (owner, operator etc)"
@@ -78,7 +135,7 @@ task(
   await getRoles(ethers.provider, addresses)
 
   console.log(`--- transfer roles: start ---`)
-  // TODO
+  await bulkTransferRoleToInputtedAddress(signer, addresses, ADDRESSES_PARAMS)
   console.log(`--- transfer roles: end ---`)
 
   console.log(`> check role after transfering ---`)
