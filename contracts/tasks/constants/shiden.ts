6836adbb138d5a470640f0dd3089543489019f8f
linnefromice <linnefromice@gmail.com>
2022-04-25 16:04:08 +0900

    update shiden config

--- contracts/tasks/constants/shiden.ts
+++ contracts/tasks/constants/shiden.ts
@@ -9,33 +9,32 @@ export const shiden: ConstantsType = {
     WETH: '', // only use when using mock
   },
   kaglas: {
-    votingEscrow: '',
+    votingEscrow: '0x9790387C4330A76B3bC9DB4Ae9d80F4099f03024',
     gaugeController: '0xfe372d95BDFE7313435D539c87E68029A792997e',
     liquidityGauge: '', // only use when using mock
     minter: "0xa6358181b2753DAC5d2Ade97519E7c1A766d9c87",
     feeDistributor: '', // only use when using mock
     registry: '', // only use when using mock
-    addressProvider: '',
+    addressProvider: '0x1aceb2849e249C5403Fe1331d63587ed43C78425',
   },
   pools: [
     {
-      name: "3Pool", // (3KGL) lptoken = 0x99799cAB58C5591ee10015F12173C1104eB23225
+      name: "3Pool", // (3KGL)
       gauge: "0xe806e841ca26fF5A82E58A7A9144B7032623E4FB",
     },
     {
-      name: "Starlay 3Pool", // (l3KGL) lptoken = 0x3328E82bB102809e91A072851a5BcBa86215E5E4
+      name: "Starlay 3Pool", // (l3KGL)
       gauge: "0xc020e5D53Af59b0Fd22970f9851AcB1a12A317c6",
     },
     {
-      name: "BUSD+3KGL", // (BUSD3KGL) lptoken = 0xaA85Ebb591077e11246422ea941c503b473b5299
+      name: "BUSD+3KGL", // (BUSD3KGL)
       gauge: "0x02871ff0b539E04A952e1d6cB4ae2f6eBCE7f3eD",
     }
   ],
   contracts: {
     treasury: {
-      address: '', // set TreasuryFunds address after deployed
+      address: '0xd582486E520D423d93658d56654b6Ef10dbeBd3A', // set TreasuryFunds address after deployed
       operator: '' // if possible, set TreasuryFunds's operator before deployed
-
     },
     muKglRewards: {
       uid: 0,
