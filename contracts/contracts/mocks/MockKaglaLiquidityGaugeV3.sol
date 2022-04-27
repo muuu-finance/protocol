4753fa1876f8e816e7d2c33f621b89ea652b9502
linnefromice <linnefromice@gmail.com>
2022-04-27 14:30:24 +0900

    fix mocks

--- contracts/contracts/mocks/MockKaglaLiquidityGaugeV3.sol
+++ contracts/contracts/mocks/MockKaglaLiquidityGaugeV3.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.12;
 // refs
 // - https://curve.readthedocs.io/dao-gauges.html#liquiditygaugev3
 contract MockKaglaLiquidityGaugeV3 {
-  function rewards_receiver() external pure returns (address) {
-    return address(0);
+  function rewards_receiver(address _address) external pure returns (address) {
+    return _address;
   }
 }
