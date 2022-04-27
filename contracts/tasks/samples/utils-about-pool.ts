72d7c3ff3c39322749564fdcda5075f3d890af7d
linnefromice <linnefromice@gmail.com>
2022-04-27 17:42:49 +0900

    update task for local test

--- contracts/tasks/samples/utils-about-pool.ts
+++ contracts/tasks/samples/utils-about-pool.ts
@@ -7,10 +7,10 @@ const SUPPORTED_NETWORK = ["localhost"] as const
 type SupportedNetwork = typeof SUPPORTED_NETWORK[number]
 
 const POOL_NAMES = [
-  "new 3Pool",
-  "new Starlay 3Pool",
-  "new BUSD+3KGL",
-  "new astridDAO",
+  "3Pool",
+  "Starlay 3Pool",
+  "BUSD+3KGL",
+  "astridDAO",
 ] 
 
 task(
@@ -74,3 +74,35 @@ const confirmCurrentPools = async (instance: Booster) => {
   }
   console.log(results)
 }
+
+task(
+  "test-utils:create-gauge:local",
+  'test-utils:create-gauge:local'
+).setAction(async ({}, hre: HardhatRuntimeEnvironment) => {
+  const { ethers, network: _network } = hre
+  if (!(SUPPORTED_NETWORK as ReadonlyArray<string>).includes(_network.name)) throw new Error(`Support only ${SUPPORTED_NETWORK} ...`)
+  const networkName = _network.name as SupportedNetwork
+  const _deployer = (await ethers.getSigners())[0]
+
+  console.log(`--- [test-utils:create-gauge:local] START ---`)
+  console.log(`network name ... ${networkName}`)
+  console.log(`deployer ... ${_deployer.address}`)
+
+  const results = []
+  for await (const [index, poolName] of POOL_NAMES.entries()) {
+    const lpToken = await new MintableERC20__factory(_deployer).deploy(
+      `New ${poolName} Token`,
+      `SYMBOL${index + 1}_NEW`,
+      18
+    )
+    await lpToken.deployTransaction.wait()
+    const gauge = await new MockKaglaGauge__factory(_deployer).deploy(lpToken.address)
+    await gauge.deployTransaction.wait()
+    results.push({
+      gauge: gauge.address,
+      lpToken: lpToken.address
+    })
+  }
+  console.log(results)
+  console.log(`--- [test-utils:create-gauge:local] FINISH ---`)
+})
