9f950160902c383a0708ff939b6fff8143faa387
Shuto kikuchi <mizoren8@gmail.com>
2022-06-09 18:54:14 +0900

    update: addresses

--- contracts/tasks/constants/shiden.ts
+++ contracts/tasks/constants/shiden.ts
@@ -3,47 +3,46 @@ import { ConstantsType } from './types'
 
 export const shiden: ConstantsType = {
   tokens: {
-    KGL: '0x039F7Ef55595923C78c2c3bBa41625eBa6F667b9',
+    KGL: '0x977b4c31DfFE505B04D09c1CC70F64339E7C1EA4',
     '3Kgl': '', // only use when using mock
     DAI: '', // only use when using mock
     WETH: '', // only use when using mock
   },
   kaglas: {
-    votingEscrow: '0xDFe3C797977a0B40C90E7c2869407327a4208654',
-    gaugeController: '0xBEDcfA1EB6cf39dd829207147692C0eaeCe32065',
+    votingEscrow: '0x05D034Ba28d5050C80DbECc36d5256575fd42431',
+    gaugeController: '0x7a1D9E204b09beC04F9036b409ffF15452974059',
     liquidityGauge: '', // only use when using mock
-    minter: "0x5dE0CF708F7753F176F1d23229c0EE50a23872f7",
+    minter: '0x0Ac67947735a9642c8d15f3421Af346D3033B5dB',
     feeDistributor: '', // only use when using mock
     registry: '', // only use when using mock
-    addressProvider: '0x762b149eA23070d6F021F70CB8877d2248278855',
+    addressProvider: '0xF86360a930590BaE72E42eFE03eba0aD61EAD6A9',
   },
   pools: [
     {
-      name: "3Pool",
-      gauge: "0x6A892edcFfafe4F64896419b1b57965e8e5bb68e",
+      name: '3Pool',
+      gauge: '0x3F41c725DD5c1a75479Ed9DB1fa8Eac77A6f76E9',
     },
     {
-      name: "Starlay 3Pool",
-      gauge: "0x1571943e281f8C579Fcf63cBD31E425c0bFDdc74",
+      name: 'Starlay 3Pool',
+      gauge: '0x3B1dfc5993aB7BC90e6FBe18221e76CF0318A577',
     },
     {
-      name: "BUSD+3KGL",
-      gauge: "0xdF180f31739284a1A8Ba3a110cDdaD58642F3DAF",
+      name: 'BUSD+3KGL',
+      gauge: '0xF05287d053546b3cc767175Db11A40393febcc4f',
     },
     {
-      name: "BAI+3KGL",
-      gauge: "0x9D2070D930005553D2994A202BB17C80053A4e00",
+      name: 'BAI+3KGL',
+      gauge: '0xad8599d608dF5492950150E6F6918196942Ef36D',
     },
     {
-      name: "oUSD+3KGL",
-      gauge: "0xA52f1d4362f4fF6A976c379C8d450AA7EB20aff4",
+      name: 'oUSD+3KGL',
+      gauge: '0xF827a7d5fED43dDA71A1f332d58f6bD433C3F75B',
     },
   ],
   contracts: {
     treasury: {
-      address: '0xCdfc500F7f0FCe1278aECb0340b523cD55b3EBbb', // set TreasuryFunds address after deployed
-      operator: '' // if possible, set TreasuryFunds's operator before deployed
-
+      address: '0xF37b07D66CE2B43fA6FCc156880D4BF3F3e0D249', // set TreasuryFunds address after deployed
+      operator: '', // if possible, set TreasuryFunds's operator before deployed
     },
     muKglRewards: {
       uid: 0,
