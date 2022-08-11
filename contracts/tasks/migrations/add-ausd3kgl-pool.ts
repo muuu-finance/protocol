2b10646218000862ddaa76b588ae1bfd73b7cd16
linnefromice <linnefromice@gmail.com>
2022-08-12 00:13:06 +0900

    feat(task): create task to add ausd3kgl pool & set gauge address in astar

---
+++ contracts/tasks/migrations/add-ausd3kgl-pool.ts
@@ -0,0 +1,46 @@
+import { task } from 'hardhat/config'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+type EthereumAddress = `0x${string}`
+
+// Parameters
+const POOL_NAME = "ausd3kgl"
+const GAUGE: { astar: EthereumAddress, shiden: EthereumAddress, localhost: EthereumAddress } = {
+  astar: "0xe6bA40C9686a05a2841829f98b5dF9cd6E3300A9",
+  shiden: "0xTBD",
+  localhost: "0xTBD"
+}
+
+/**
+ * Template task to add pool to protocol
+ * - This task takes responsibility to convert parameter about adding pool
+ *   - Transfer actual processing to `add-pool-extended-version` task
+ * - Operator need
+ *   - modify Parameters (defined above)
+ *   - update constants (key is `pools`) for each network
+ */
+task(
+  `add-${POOL_NAME}-pool`,
+  'add pool to expand add-pool migration'
+)
+.addOptionalParam('deployerAddress', "Deployer's address")
+.setAction(async ({ deployerAddress }: { deployerAddress: string }, hre: HardhatRuntimeEnvironment) => {
+  console.log(`--- [add-${POOL_NAME}-pool] START ---`)
+  const { ethers, network: _network } = hre
+
+  const _deployer =
+    (await ethers.getSigner(deployerAddress)) ||
+    (await ethers.getSigners())[0]
+
+  const networkName = _network.name as keyof typeof GAUGE
+  const gauge = GAUGE[networkName]
+  if (gauge == null) throw new Error(`[ERROR] gauge's address is null`)
+
+  await hre.run(`add-pool-extended-version`, {
+    deployerAddress: _deployer.address,
+    poolName: POOL_NAME,
+    gaugeAddress: gauge,
+    networkName: networkName
+  })
+  console.log(`--- [add-${POOL_NAME}-pool] FINISHED ---`)
+})
