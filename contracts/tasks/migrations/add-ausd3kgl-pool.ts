9eafda9702b4d721ef8c749cb883677400da6987
linnefromice <linnefromice@gmail.com>
2022-08-16 14:19:45 +0900

    update: task to add ausd3kgl - use forceAdd

--- contracts/tasks/migrations/add-ausd3kgl-pool.ts
+++ contracts/tasks/migrations/add-ausd3kgl-pool.ts
@@ -5,10 +5,25 @@ type EthereumAddress = `0x${string}`
 
 // Parameters
 const POOL_NAME = "ausd3kgl"
-const GAUGE: { astar: EthereumAddress, shiden: EthereumAddress, localhost: EthereumAddress } = {
-  astar: "0xe6bA40C9686a05a2841829f98b5dF9cd6E3300A9",
-  shiden: "0xTBD",
-  localhost: "0xTBD"
+type Param = {
+  gauge: EthereumAddress | null
+  lpToken: EthereumAddress | null
+}
+const PARAMS: {
+  astar: Param, shiden: Param, localhost: Param
+} = {
+  astar: {
+    gauge: "0xe6bA40C9686a05a2841829f98b5dF9cd6E3300A9",
+    lpToken: null
+  },
+  shiden: {
+    gauge: null,
+    lpToken: null
+  },
+  localhost: {
+    gauge: null,
+    lpToken: null
+  }
 }
 
 /**
@@ -32,8 +47,8 @@ task(
     (await ethers.getSigner(deployerAddress)) ||
     (await ethers.getSigners())[0]
 
-  const networkName = _network.name as keyof typeof GAUGE
-  const gauge = GAUGE[networkName]
+  const networkName = _network.name as keyof typeof PARAMS
+  const gauge = PARAMS[networkName].gauge
   if (gauge == null) throw new Error(`[ERROR] gauge's address is null`)
 
   await hre.run(`add-pool-extended-version`, {
@@ -43,4 +58,32 @@ task(
     networkName: networkName
   })
   console.log(`--- [add-${POOL_NAME}-pool] FINISHED ---`)
+})
+
+task(
+  `force-add-${POOL_NAME}-pool`,
+  'add pool to expand add-pool migration'
+)
+.addOptionalParam('deployerAddress', "Deployer's address")
+.setAction(async ({ deployerAddress }: { deployerAddress: string }, hre: HardhatRuntimeEnvironment) => {
+  console.log(`--- [force-add-${POOL_NAME}-pool] START ---`)
+  const { ethers, network: _network } = hre
+
+  const _deployer =
+    (await ethers.getSigner(deployerAddress)) ||
+    (await ethers.getSigners())[0]
+
+  const networkName = _network.name as keyof typeof PARAMS
+  const param = PARAMS[networkName]
+  if (param.gauge == null) throw new Error(`[ERROR] gauge's address is null`)
+  if (param.lpToken == null) throw new Error(`[ERROR] lpToken's address is null`)
+
+  await hre.run(`force-add-pool-extended-version`, {
+    deployerAddress: _deployer.address,
+    poolName: POOL_NAME,
+    gaugeAddress: param.gauge,
+    lpTokenAddress: param.lpToken,
+    networkName: networkName
+  })
+  console.log(`--- [force-add-${POOL_NAME}-pool] FINISHED ---`)
 })
