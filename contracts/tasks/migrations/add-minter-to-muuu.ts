fd175cc5655e769b1780079485712a7d7f1f8cd3
linnefromice <linnefromice@gmail.com>
2022-08-29 00:12:46 +0900

    clean: tasks to use contract's setter

--- contracts/tasks/migrations/add-minter-to-muuu.ts
+++ contracts/tasks/migrations/add-minter-to-muuu.ts
@@ -3,7 +3,16 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { MuuuToken__factory } from '../../types'
 import { TaskUtils } from '../utils'
 
-const NEW_MINTER: string = ''
+type EthereumAddress = `0x${string}`
+const PARAMS: {
+  astar: EthereumAddress
+  shiden: EthereumAddress
+  localhost: EthereumAddress
+} = {
+  astar: '0xTBD',
+  shiden: '0xTBD',
+  localhost: '0xTBD',
+}
 
 task('add-minter-to-muuu', 'MuuuToken#addMinter')
   .addOptionalParam('executor', "executor's address")
@@ -15,6 +24,7 @@ task('add-minter-to-muuu', 'MuuuToken#addMinter')
       const { ethers, network } = hre
       const _executor =
         (await ethers.getSigner(executor)) || (await ethers.getSigners())[0]
+      const newMinter = PARAMS[network.name as keyof typeof PARAMS]
 
       console.log(`--- [add-minter-to-muuu] START ---`)
       const { system } = TaskUtils.loadDeployedContractAddresses({
@@ -24,18 +34,18 @@ task('add-minter-to-muuu', 'MuuuToken#addMinter')
 
       console.log(
         `[BEFORE] Booster#minterList(new minter): ${await muuu.minterList(
-          NEW_MINTER,
+          newMinter,
         )}`,
       )
       console.log()
 
-      const tx = await muuu.addMinter(NEW_MINTER)
+      const tx = await muuu.addMinter(newMinter)
       console.log(`tx: ${tx.hash}`)
       await tx.wait()
 
       console.log(
         `[AFTER] Booster#minterList(new minter): ${await muuu.minterList(
-          NEW_MINTER,
+          newMinter,
         )}`,
       )
       console.log()
--- contracts/tasks/migrations/set-reward-multiplier.ts
+++ contracts/tasks/migrations/set-reward-multiplier.ts
@@ -3,7 +3,15 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { Booster__factory } from '../../types'
 import { TaskUtils } from '../utils'
 
-const REWARD_MULTIPLIER: number = 0
+const PARAMS: {
+  astar: number
+  shiden: number
+  localhost: number
+} = {
+  astar: 0,
+  shiden: 0,
+  localhost: 0,
+}
 
 task('set-reward-multiplier', 'Booster#setRewardMultiplier')
   .addOptionalParam('executor', "executor's address")
@@ -14,23 +22,31 @@ task('set-reward-multiplier', 'Booster#setRewardMultiplier')
     ) => {
       const { ethers, network } = hre
       const _executor =
-        (await ethers.getSigner(executor)) ||
-        (await ethers.getSigners())[0]
+        (await ethers.getSigner(executor)) || (await ethers.getSigners())[0]
+      const rewardMultiplier = PARAMS[network.name as keyof typeof PARAMS]
 
       console.log(`--- [set-reward-multiplier] START ---`)
       const { system } = TaskUtils.loadDeployedContractAddresses({
-        network: network.name
+        network: network.name,
       })
       const booster = Booster__factory.connect(system.booster, _executor)
 
-      console.log(`[BEFORE] Booster#rewardMultiplier: ${(await booster.rewardMultiplier()).toNumber()}`)
+      console.log(
+        `[BEFORE] Booster#rewardMultiplier: ${(
+          await booster.rewardMultiplier()
+        ).toNumber()}`,
+      )
       console.log()
 
-      const tx = await booster.setRewardMultiplier(REWARD_MULTIPLIER)
+      const tx = await booster.setRewardMultiplier(rewardMultiplier)
       console.log(`tx: ${tx.hash}`)
       await tx.wait()
 
-      console.log(`[AFTER] Booster#rewardMultiplier: ${(await booster.rewardMultiplier()).toNumber()}`)
+      console.log(
+        `[AFTER] Booster#rewardMultiplier: ${(
+          await booster.rewardMultiplier()
+        ).toNumber()}`,
+      )
       console.log()
 
       console.log(`--- [set-reward-multiplier] FINISHED ---`)
